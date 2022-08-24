6574b5618aad07be410849b7c9920643d65013ba
Boubajoker <mohanathas.leo@gmail.com>
2022-08-24 19:51:26 +0200

    v0.0.2 Alpha A-1
    - Added Some Feature !

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 - v0.0.1 Alpha A-1 {master} {
     - Inital Commit, Added module files into the repository.
 }
+- v0.0.2 Alpha A-1 {master} {
+    - Added Some Feature !
+}
--- searchgpy/__init__.py
+++ searchgpy/__init__.py
@@ -15,6 +15,7 @@ class GoogleSearchEngine:
             pass
         else:
             webbrowser.open(self.base_google_search_url)
+            print('Redirecting to URL:', self.base_google_search_url)
 
     def search_img(self, text, downlaod=False) -> Any:
         self.base_google_img_url = 'https://www.google.com/search?q=' + text + '&sxsrf=ALiCzsYlU6o2Uq4WIlhJlkHR20Dh5VFItQ:1661285028951&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjXupWA4d35AhVE-YUKHQIaBFIQ_AUoAXoECAIQAw&biw=1879&bih=1008&dpr=1'
@@ -23,6 +24,7 @@ class GoogleSearchEngine:
             pass
         else:
             webbrowser.open(self.base_google_img_url)
+            print('Redirecting to URL:', self.base_google_img_url)
     
     def search_news(self, text) -> Any:
         self.base_google_news_url = 'https://www.google.com/search?q=' + text + '!&source=lmns&tbm=nws&bih=990&biw=909&hl=fr&sa=X&ved=2ahUKEwj5zdSq4d_5AhUfQvEDHYvQAxUQ_AUoA3oECAEQAw'
@@ -30,6 +32,8 @@ class GoogleSearchEngine:
         if text == None:
             pass
         else:
-            webbrowser.open(self.base_google_news_url)    
+            webbrowser.open(self.base_google_news_url)
+            print('Redirecting to URL:', self.base_google_news_url)
+             
 if __name__ == '__main__':
     webbrowser.open('https://boubajoker.github.io/SearchGooglePy/?link=Home')
